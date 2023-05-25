a6c49d551e7b01202d9023c19369524ec982b36e
popomon <popomonclub@gmail.com>
2023-05-26 08:34:06 +0900

    [WIP]: Added default timer interface

--- src/MomentScreen/components/DefaultTimer.tsx
+++ src/MomentScreen/components/DefaultTimer.tsx
@@ -1,28 +1,60 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import useDefaultTimer from '../hooks/useDefaultTimer';
 
 interface Props {}
 
-const DefaultTimer = ({}: Props) => {
+const CurrentTimer = ({}: Props) => {
   const styles = useStyles();
   const defaultTimer = useDefaultTimer();
+  const [timerState, setTimerState] = useState<'ready' | 'paused' | 'started'>(
+    'ready',
+  );
 
-  useEffect(() => {
-    defaultTimer.start();
-    return () => {
-      defaultTimer.stop();
-    };
-  }, []);
+  const handleStart = () => {
+    setTimerState('started');
+    if (timerState === 'ready') {
+      defaultTimer.start();
+    } else {
+      defaultTimer.restart();
+    }
+  };
+
+  const handlePause = () => {
+    setTimerState('paused');
+    defaultTimer.stop();
+  };
+
+  const handleReset = () => {
+    setTimerState('ready');
+    defaultTimer.reset();
+  };
 
   return (
     <View>
       <Text style={styles.time}>{defaultTimer.time}</Text>
+      <View>
+        {['ready', 'paused'].includes(timerState) && (
+          <TouchableOpacity onPress={handleStart}>
+            <Text>시작</Text>
+          </TouchableOpacity>
+        )}
+        {['started'].includes(timerState) && (
+          <TouchableOpacity onPress={handlePause}>
+            <Text>중지</Text>
+          </TouchableOpacity>
+        )}
+        {['paused'].includes(timerState) && (
+          <TouchableOpacity onPress={handleReset}>
+            <Text>리셋</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
 
-export default DefaultTimer;
+export default CurrentTimer;
 
 const useStyles = () =>
   StyleSheet.create({
--- src/MomentScreen/hooks/useDefaultTimer.ts
+++ src/MomentScreen/hooks/useDefaultTimer.ts
@@ -5,31 +5,55 @@ const useDefaultTimer = () => {
   const limit = 30;
   const timer = useRef<number>(0);
   const [time, setTime] = useState(moment());
+  const [startTime, setStartTime] = useState(moment());
 
-  const startTime = useRef(moment());
-  const start = () => {
-    startTime.current = moment();
-    timer.current = setInterval(() => {
+  const _updateTime = () => {
+    timer.current = setTimeout(() => {
       setTime(moment());
-    }, 1000);
+      _updateTime();
+    }, 100);
+  };
+
+  const start = () => {
+    setStartTime(moment());
+    _updateTime();
+  };
+
+  const restart = () => {
+    const duration = time.diff(startTime, 'millisecond');
+    setStartTime(() => moment().add(-duration, 'millisecond'));
+    setTime(() => moment());
+    _updateTime();
+  };
+
+  const pause = () => {
+    clearTimeout(timer.current);
+  };
+
+  const reset = () => {
+    setStartTime(moment());
+    setTime(moment());
   };
 
   const stop = () => {
     clearInterval(timer.current);
   };
 
-  const defaultTime = (second: number): string => {
+  const defaultTime = (seconds: number): string => {
     const TIME_FORMAT = 'HH:mm:ss';
-    if (second < 0) {
-      return '+' + moment.utc(-1 * second * 1000).format(TIME_FORMAT);
+    if (seconds < 0) {
+      return '+' + moment.utc(-1 * seconds * 1000).format(TIME_FORMAT);
     } else {
-      return moment.utc(second * 1000).format(TIME_FORMAT);
+      return moment.utc(seconds * 1000).format(TIME_FORMAT);
     }
   };
 
   return {
-    time: defaultTime(limit - time.diff(startTime.current, 'seconds')),
+    time: defaultTime(limit - time.diff(startTime, 'second')),
     start,
+    restart,
+    pause,
+    reset,
     stop,
   };
 };
